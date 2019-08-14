c2cb00eb7e8909c5d7839ceb2783c8770618ac23
ericpinera <eepinera.emp@gmail.com>
2019-08-14 15:34:57 -0500

    Paciente

--- src/main/java/com/eryusoft/MediConsult/Controladores/PacienteControlador.java
+++ src/main/java/com/eryusoft/MediConsult/Controladores/PacienteControlador.java
@@ -2,13 +2,25 @@ package com.eryusoft.MediConsult.Controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.eryusoft.MediConsult.Model.Paciente;
 
 @Controller
+@RequestMapping("/paciente")
 public class PacienteControlador 
 {
-	@GetMapping("/paciente")
+	@GetMapping(path = {"","/"})
 	public String paciente() 
 	{
+		return "Paciente";
+	}
+	
+	@RequestMapping("registrarPaciente")
+	public String registrarPaciente(Paciente paciente) 
+	{
+		System.out.println(paciente.getNombresApellidos());
 		return "Paciente";
 	}
 
